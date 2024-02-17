0b6f93d57433080e16347de3672d3d685935e1bb
Timothy <timothyliong05@gmail.com>
2024-02-17 20:33:28 +0800

    preliminary added html for the page

--- index.php
+++ index.php
@@ -6,7 +6,46 @@
     <title>Main page</title>
 </head>
 <body>
-    <h1>Hello World!</h1>
-    hello
+    <h1>Top Creations</h1>
+    <div class="top-one">
+        <h2>1</h2>
+        <a href="top1"><img src="" alt="Top creation"></a>
+        <h3>14</h3>
+        <img src="vote" alt="vote">
+        <img src="others" alt="reaction">
+        <img src="... more" alt="other reactions">
+    </div>
+    <div class="top-recent-qrcode-wrapper">
+        <div class="top-two">
+            <h3>2</h3>
+            <a href="top2"><img src="" alt="Top second creation"></a>
+            <h4>14</h4>
+            <img src="vote" alt="vote">
+            <img src="others" alt="reaction">
+            <img src="... more" alt="other reactions">
+        </div>
+        <div class="top-three">
+            <h3>2</h3>
+            <a href="top3"><img src="" alt="Top third creation"></a>
+            <h4>14</h4>
+            <img src="vote" alt="vote">
+            <img src="others" alt="reaction">
+            <img src="... more" alt="other reactions">
+        </div>
+        <div class="qr-code">
+            <h2>Vote for your favourite creations!</h2>
+            <img src="" alt="qr code for voting">
+        </div>
+        <div class="recent-creations">
+            <h2>Recently made creations</h2>
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+        </div>
+    </div>
+    
+    
 </body>
 </html>

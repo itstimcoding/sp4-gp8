945dafd1cbc4be250b4370cc03e7a386c8a245a1
Timothy <timothyliong05@gmail.com>
2024-02-20 05:45:49 +0800

    changed order of top 3 html to show 2,1 ,3

--- controller/top3_fetch_data.php
+++ controller/top3_fetch_data.php
@@ -15,17 +15,8 @@
     mysqli_free_result($mysqli_result);
 ?>
 
-<div class="top-one">
-<h2>1</h2>
-<a href="top1"><img src="<?php echo $top3_creations_array[0]['image_url']?>" alt="Top creation"></a>
-<div class="reactions">
-    <h3><?php echo $top3_creations_array[0]['likes']?></h3>
-    <img src="vote" alt="vote">
-    <img src="others" alt="reaction">
-    <img src="... more" alt="other reactions">  
-</div>
-</div>
-<div class="top-2n3-wrapper">
+
+<div class="top3-wrapper">
     <a href="top2">
         <div class="top-two">
             <h3>2</h3>
@@ -36,6 +27,19 @@
             <img src="... more" alt="other reactions">
         </div>
     </a>
+    <a href="top1">
+    <div class="top-one">
+        <h2>1</h2>
+        <a href="top1"><img src="<?php echo $top3_creations_array[0]['image_url']?>" alt="Top creation"></a>
+        <div class="reactions">
+            <h3><?php echo $top3_creations_array[0]['likes']?></h3>
+            <img src="vote" alt="vote">
+            <img src="others" alt="reaction">
+            <img src="... more" alt="other reactions">  
+        </div>
+    </div>
+    </a>
+    
     <a href="top3">
         <div class="top-three">
             <h3>2</h3>

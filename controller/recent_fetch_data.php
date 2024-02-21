4e6e889759f0f46b7c77a13a1c3be656a41e03d0
Timothy <timothyliong05@gmail.com>
2024-02-22 02:12:39 +0800

    allowing tapping of tapping on recent posts to view in detail

--- controller/recent_fetch_data.php
+++ controller/recent_fetch_data.php
@@ -13,14 +13,16 @@
 
 ?>
 
-<?php foreach($recent_creations_array as $recent_creation) { ?>  
-    <div class="recent">
-        <img src="uploads/<?php echo $recent_creation['image_url']?>" alt="recent creation"> 
-        <div class="recent-reactions">
-            <h5><?php echo $recent_creation['likes']?> reacts</h5>
-            <h5><?php echo time_elapsed_string($recent_creation['created'])?></h5>
-        </div>
-    </div>            
+<?php foreach($recent_creations_array as $recent_creation) { ?>
+    <a href="creation.php?creation_id=<?php echo $recent_creation['id']?>"> 
+        <div class="recent">
+            <img src="uploads/<?php echo $recent_creation['image_url']?>" alt="recent creation"> 
+            <div class="recent-reactions">
+                <h5><?php echo $recent_creation['likes']?> reacts</h5>
+                <h5><?php echo time_elapsed_string($recent_creation['created'])?></h5>
+            </div>
+        </div>  
+    </a>   
 <?php } ?>
 
 
